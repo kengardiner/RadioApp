99f1eaa7c01b4dfdba6dd4c9f27127fc4c71102e
kengardiner <ken_gardiner@yahoo.com>
2016-08-25 21:55:46 -0400

    Adapter in place

---
+++ app/src/main/java/com/cybermoosemedia/radioapp/adapters/StationsAdapter.java
@@ -0,0 +1,28 @@
+package com.cybermoosemedia.radioapp.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.cybermoosemedia.radioapp.holders.StationViewHolder;
+
+/**
+ * Created by ken_g on 8/25/2016.
+ * Grabs data for view
+ */
+public class StationsAdapter extends RecyclerView.Adapter<StationViewHolder> {
+
+    @Override
+    public void onBindViewHolder(StationViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public StationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+}
---
+++ app/src/main/java/com/cybermoosemedia/radioapp/holders/StationViewHolder.java
@@ -0,0 +1,14 @@
+package com.cybermoosemedia.radioapp.holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by ken_g on 8/25/2016.
+ */
+public class StationViewHolder extends RecyclerView.ViewHolder {
+
+    public StationViewHolder(View itemView) {
+        super(itemView);
+    }
+}
